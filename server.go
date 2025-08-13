a6b165bd11a6aaf9704a370a0e3dad6bd6dad42e
Chris Smith <chris@chameth.com>
2025-08-13 12:23:34 +0100

    Fix encoding issues

--- server.go
+++ server.go
@@ -87,7 +87,7 @@ func (s *Server) handleCSSServe(w http.ResponseWriter, r *http.Request) {
 	}
 
 	css := s.generateCSS(fontName, metadata)
-	w.Header().Set("Content-Type", "text/css")
+	w.Header().Set("Content-Type", "text/css; charset=utf-8")
 	w.Write([]byte(css))
 }
 
@@ -138,7 +138,7 @@ func (s *Server) handleIndex(w http.ResponseWriter, r *http.Request) {
 		return fontList[i].Name < fontList[j].Name
 	})
 
-	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if err := s.template.Execute(w, fontList); err != nil {
 		log.Printf("Error executing template: %v", err)
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
